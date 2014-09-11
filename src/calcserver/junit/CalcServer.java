fee32443544d2b7bd8d40c076f832e0c5efe654c
Divole <dvlmmm@gmail.com>
2014-09-11 12:17:00 +0200

    test example

--- src/calcserver/junit/CalcServer.java
+++ src/calcserver/junit/CalcServer.java
@@ -2,4 +2,13 @@ package calcserver.junit;
 
 public class CalcServer {
 
+    public static int compareValues(int userGuess, int correctValue){
+
+        if(userGuess == correctValue){
+            return correctValue;
+
+        }
+        return 0;
+    }
+
 }
